96a678cfec3ed26078518b7573828bb8174fee4e
Cody Crawford <zimquo@gmail.com>
2018-01-30 02:05:07 -0700

    AI started

---
+++ Source/Abeyance/BaseEnemy.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseEnemy.h"
+
+
+// Sets default values
+ABaseEnemy::ABaseEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+// Calculate death function (helper)
+void ABaseEnemy::CalculateDead()
+{
+
+	if(Health <= 0)
+		IsDead = true;
+	else
+		IsDead = false;
+}
+
---
+++ Source/Abeyance/BaseEnemy.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseEnemy.generated.h"
+
+UCLASS()
+class ABEYANCE_API ABaseEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Calculate death function (helper)
+	virtual void CalculateDead();
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadwrite, Category = "Life")
+	float Health = 100;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Life")
+	bool IsDead = false;
+
+};
